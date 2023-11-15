97a7216c744f2845bf1e34d6c4a71e91424e1517
Bryan-Nathaniel-Tutuhatunewa <bryan.tutuhatunewa@gmail.com>
2023-11-16 00:20:42 +0900

    First Commit

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
+
 from flask import Flask, render_template, request, jsonify
 import requests
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
-
-import os
-from os.path import join, dirname
-from dotenv import load_dotenv
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
